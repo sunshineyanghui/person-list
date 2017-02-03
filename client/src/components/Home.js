f35be668a9924ff86af35e0ca67e1c667ecd0971
sunshineyanghui <1187073986@qq.com>
2017-02-03 20:12:12 +0800

    home personlist

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,13 +1,28 @@
 import React from 'react';
-
+import axios from 'axios';
 class Home extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      persons: []
+    };
+  }
+  componentWillMount() {
+    axios.get('http://localhost:3000/persons')
+    .then((res) => this.setState({persons:res.data.persons}))
+  }
   render(){
+    // console.log(this.state.persons);
+    let personList = this.state.persons.map((person,i)=>(
+      <div key={i}>
+        <span>{person.name}</span><span>{person.age}</span><span>{person.sex}</span><span>{person.email}</span>
+      </div>
+    ))
     return(
       <div>
-        Home
+        {personList}
       </div>
     )
   }
- }
-
+}
 export default Home;
