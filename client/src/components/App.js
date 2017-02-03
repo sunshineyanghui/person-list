d5e43175607e3f7814d01d36ac1437d2e36bae94
sunshineyanghui <1187073986@qq.com>
2017-02-03 17:42:38 +0800

    add header footer

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import Header from './Header';
+import Footer from './Footer';
 class App extends React.Component{
   render(){
     return(
       <div>
-        App
+        <Header/>
         {this.props.children}
+        <Footer/>
       </div>
     )
   }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component{
+  render(){
+    return(
+      <div className="footer">
+        <p>BEFE coding test,2016</p>
+      </div>
+    )
+  }
+ }
+
+export default Footer;
