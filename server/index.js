745fac9336d50786f8898334fe5b7254babaa0d5
sunshineyanghui <1187073986@qq.com>
2017-02-04 21:03:10 +0800

    add a person

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
 const Person = require('./models/person');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 app.use(cors());
-
+app.use(bodyParser.json());
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/person');
 
@@ -18,9 +19,22 @@ db.once('open', function(){
 db.on('error',function (err) {
   console.log(err);
 });//on当什么时候 当报错的时候
+//首页展示所有person
 app.get('/persons', function(req,res){
   Person.find().exec(function(err, persons) {
     res.json({persons})
+  });
+})
+//向数据库添加一条
+app.post('/persons',function (req,res) {
+  res.json(req.body)
+  let name = req.body.name;
+  let age = req.body.age;
+  let sex = req.body.sex;
+  let email = req.body.email;
+ 　const person = new Person({name: name, age: age,sex:sex,email:email});
+  person.save(function(){
+    console.log('saved!');
   });
 })
 app.listen(3000,function(){
